db7a1efbfd8cf2dda47701b04d29bca38265d529
Stephen Xie <coreficent@gmail.com>
2020-12-21 21:28:06 -0500

    Implement rotation controller.

---
+++ Assets/Script/Coreficent/Controller/SphereController.cs
@@ -0,0 +1,29 @@
+﻿namespace Coreficent.Transform
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SphereController : MonoBehaviour
+    {
+        public float RotationSpeed = 100.0f;
+
+        private readonly string _verticalControl = "Vertical";
+        private readonly string _horizontalControl = "Horizontal";
+
+        private Vector3 _direction = new Vector3();
+
+        private void Update()
+        {
+            ControlRotation();
+        }
+
+        private void ControlRotation()
+        {
+            _direction.x = Input.GetAxis(_horizontalControl);
+            _direction.y = Input.GetAxis(_verticalControl);
+            transform.rotation *= Quaternion.AngleAxis(_direction.normalized.x * RotationSpeed * Time.deltaTime, transform.InverseTransformDirection(Vector3.down));
+            transform.rotation *= Quaternion.AngleAxis(_direction.normalized.y * RotationSpeed * Time.deltaTime, transform.InverseTransformDirection(Vector3.right));
+        }
+    }
+}
