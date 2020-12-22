1ec6a3d3065cf4ba2aa40753eebe6a7b32830e16
Stephen Xie <coreficent@gmail.com>
2020-12-21 22:42:48 -0500

    Add sphere control presets.

--- Assets/Script/Coreficent/Controller/SphereController.cs
+++ Assets/Script/Coreficent/Controller/SphereController.cs
@@ -20,10 +20,41 @@
 
         private void ControlRotation()
         {
+            if (Input.GetKey(KeyCode.Alpha5) || Input.GetKey(KeyCode.Keypad5))
+            {
+                LerpTo(Vector3.back, Vector3.up);
+            }
+            if (Input.GetKey(KeyCode.Alpha0) || Input.GetKey(KeyCode.Keypad0))
+            {
+                LerpTo(Vector3.forward, Vector3.up);
+            }
+            if (Input.GetKey(KeyCode.Alpha8) || Input.GetKey(KeyCode.Keypad8))
+            {
+                LerpTo(Vector3.up, Vector3.forward);
+            }
+            if (Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Keypad2))
+            {
+                LerpTo(Vector3.down, Vector3.back);
+            }
+            if (Input.GetKey(KeyCode.Alpha4) || Input.GetKey(KeyCode.Keypad4))
+            {
+                LerpTo(Vector3.left, Vector3.up);
+            }
+            if (Input.GetKey(KeyCode.Alpha6) || Input.GetKey(KeyCode.Keypad6))
+            {
+                LerpTo(Vector3.right, Vector3.up);
+            }
+
             _direction.x = Input.GetAxis(_horizontalControl);
             _direction.y = Input.GetAxis(_verticalControl);
             transform.rotation *= Quaternion.AngleAxis(_direction.normalized.x * RotationSpeed * Time.deltaTime, transform.InverseTransformDirection(Vector3.down));
             transform.rotation *= Quaternion.AngleAxis(_direction.normalized.y * RotationSpeed * Time.deltaTime, transform.InverseTransformDirection(Vector3.right));
+        }
+
+        private void LerpTo(Vector3 forward, Vector3 upwards)
+        {
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(forward, upwards), RotationSpeed / 20 * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(forward, upwards), RotationSpeed * 2.0f * Time.deltaTime);
         }
     }
 }
