ab69b81e38d1769edb82e47153b23b36ddd06a98
Stephen Xie <coreficent@gmail.com>
2020-12-30 03:41:15 -0500

    Implement mouse click event.

--- Assets/Script/Coreficent/Interface/SpriteButton.cs
+++ Assets/Script/Coreficent/Interface/SpriteButton.cs
@@ -6,7 +6,7 @@
     using UnityEngine;
     using UnityEngine.EventSystems;
 
-    public class SpriteButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class SpriteButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
         private Animator _animator;
 
@@ -34,6 +34,16 @@
             DebugLogger.Log("on exit sprite button");
             _animator.SetBool("Enter", false);
             _animator.SetBool("Exit", true);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+                Debug.Log("Left click");
+            else if (eventData.button == PointerEventData.InputButton.Middle)
+                Debug.Log("Middle click");
+            else if (eventData.button == PointerEventData.InputButton.Right)
+                Debug.Log("Right click");
         }
     }
 }
