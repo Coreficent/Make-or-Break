8053f5ae65f8e1e153f1b7d6238a7fe06b7a2b76
Stephen Xie <coreficent@gmail.com>
2020-12-30 03:34:25 -0500

    Implement sprite button animation.

---
+++ Assets/Script/Coreficent/Interface/SpriteButton.cs
@@ -0,0 +1,39 @@
+﻿namespace Coreficent.Interface
+{
+    using Coreficent.Utility;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    public class SpriteButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+
+            SanityCheck.Check(this, _animator);
+        }
+
+        private void Update()
+        {
+
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            DebugLogger.Log("on enter sprite button");
+            _animator.SetBool("Enter", true);
+            _animator.SetBool("Exit", false);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            DebugLogger.Log("on exit sprite button");
+            _animator.SetBool("Enter", false);
+            _animator.SetBool("Exit", true);
+        }
+    }
+}
