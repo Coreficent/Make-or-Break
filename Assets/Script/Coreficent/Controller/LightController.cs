a9b270c4a58f383ac849a1f2b8cf702967c46f97
Stephen Xie <coreficent@gmail.com>
2021-01-07 12:51:12 -0500

    Implement light controller.

---
+++ Assets/Script/Coreficent/Controller/LightController.cs
@@ -0,0 +1,23 @@
+﻿namespace Coreficent.Controller
+{
+    using Coreficent.Utility;
+    using UnityEngine;
+
+    public class LightController : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed;
+
+        private Vector3 _eulerRotation = Vector3.zero;
+        protected void Start()
+        {
+            SanityCheck.Check(this, _rotationSpeed);
+
+            _eulerRotation.x += _rotationSpeed;
+        }
+
+        protected void Update()
+        {
+            transform.Rotate(_eulerRotation * Time.deltaTime);
+        }
+    }
+}
