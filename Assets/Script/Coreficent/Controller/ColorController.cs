dd17b91049e8c80c37883badf3af2d904500566a
Stephen Xie <coreficent@gmail.com>
2021-01-07 20:03:47 -0500

    Implement color controller.

---
+++ Assets/Script/Coreficent/Controller/ColorController.cs
@@ -0,0 +1,26 @@
+﻿namespace Coreficent.Controller
+{
+    using Coreficent.Utility;
+    using UnityEngine;
+
+    public class ColorController : MonoBehaviour
+    {
+        [SerializeField] private Material _material;
+        [SerializeField] private Color _color = Color.clear;
+
+        protected void Start()
+        {
+            SanityCheck.Check(this, _material);
+
+            DebugLogger.Start(this);
+        }
+
+        protected void Update()
+        {
+            if (!Color.clear.Equals(_color))
+            {
+                _material.SetColor("_Color", _color);
+            }
+        }
+    }
+}
