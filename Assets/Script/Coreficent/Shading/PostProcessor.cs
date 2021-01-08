4710e9b9d0d5b6219a5c189604ada899dc45d38b
Stephen Xie <coreficent@gmail.com>
2021-01-08 09:27:59 -0500

    Implement post processor.

---
+++ Assets/Script/Coreficent/Shading/PostProcessor.cs
@@ -0,0 +1,26 @@
+﻿namespace Coreficent.Shading
+{
+    using Coreficent.Utility;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PostProcessor : MonoBehaviour
+    {
+        [SerializeField] private Material _material;
+        protected void Start()
+        {
+            SanityCheck.Check(this, _material);
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination, _material);
+        }
+    }
+}
