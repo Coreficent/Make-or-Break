afad26752701b2d15171eabc813ca294ecccbc05
Stephen Xie <coreficent@gmail.com>
2021-01-11 15:08:54 -0500

    Implement background music.

---
+++ Assets/Script/Coreficent/Audio/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Coreficent.Audio
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Singleton = null;
+
+        protected void awake()
+        {
+            Singleton = this;
+        }
+    }
+}
---
+++ Assets/Script/Coreficent/Audio/BackgroundMusic.cs
@@ -0,0 +1,14 @@
+﻿namespace Coreficent.Audio
+{
+    using UnityEngine;
+
+    public class BackgroundMusic : MonoBehaviour
+    {
+        public AudioSource Music;
+
+        protected void Start()
+        {
+            Music.Play();
+        }
+    }
+}
